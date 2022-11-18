4b0a5372441f5851774ead5d7878e042538ccc56
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-11-18 22:53:20 +0800

    Add Pause function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,30 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public Player player;
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+
     private int score;
+
+    private void Awake() {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play() {
+        
+    }
+
+    public void Pause() {
+        // simulate pause
+        Time.timeScale = 0f;
+
+        player.enabled = false;
+    }
 
     public void IncreaseScore() {
         score++;
