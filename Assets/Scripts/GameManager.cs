7d6b66b1980c3216776b34ceb400fd3916f4680d
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-11-18 23:46:13 +0800

    Add Get ready display

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
@@ -7,6 +8,9 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public GameObject playButton;
     public GameObject gameOver;
+    public GameObject getReady;
+
+    public float delayTime = 1f;
 
     private int score;
 
@@ -21,15 +25,22 @@ public class GameManager : MonoBehaviour
 
         gameOver.SetActive(false);
         playButton.SetActive(false);
+        getReady.SetActive(true);
 
         Time.timeScale = 1f;
-        player.enabled = true;
+
+        // reset the position of the player when enabled
+        Vector3 position = player.transform.position;
+        position.y = 0f;
+        player.transform.position = position;
 
         // Destroy all obstacles in the screen
         Obstacles[] obstacles = FindObjectsOfType<Obstacles>();
         for (int i = 0; i < obstacles.Length; i++) {
             Destroy(obstacles[i].gameObject);
         }
+
+        StartCoroutine(delaySpawn(delayTime));
     }
 
     public void Pause() {
@@ -50,5 +61,13 @@ public class GameManager : MonoBehaviour
         playButton.SetActive(true);
 
         Pause();
+    }
+
+    IEnumerator delaySpawn(float delayTime) {
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        yield return new WaitForSeconds(delayTime);
+        getReady.SetActive(false);
+        player.enabled = true;
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,6 +21,10 @@ public class Player : MonoBehaviour
         InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
     }
 
+    private void OnEnable() {
+        direction = Vector3.zero;
+    }
+
     private void Update() {
         // key or click input
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
